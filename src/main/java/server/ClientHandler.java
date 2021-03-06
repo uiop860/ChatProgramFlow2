45ba4297ac38a3c5a92be6b36b05cfb3f9b42696
uiop860 <github+olivertaehr96@gmail.com>
2021-03-06 14:49:20 +0100

    Command CLOSE, SEND, CONNECT implementet

--- src/main/java/server/ClientHandler.java
+++ src/main/java/server/ClientHandler.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ClientHandler implements Runnable {
 
@@ -11,10 +12,14 @@ public class ClientHandler implements Runnable {
     private PrintWriter pw;
     private Scanner scanner;
     private String ip;
+    private ConcurrentHashMap<String,String> userList;
+    private Server server;
 
-    public ClientHandler(Socket socket, String ip) throws IOException {
+    public ClientHandler(Socket socket, String ip, ConcurrentHashMap<String,String> userList,Server server) throws IOException {
         this.socket = socket;
         this.ip = ip;
+        this.userList = userList;
+        this.server = server;
         this.pw = new PrintWriter(socket.getOutputStream());
         this.scanner = new Scanner(socket.getInputStream());
     }
@@ -29,26 +34,64 @@ public class ClientHandler implements Runnable {
         boolean keepRunning = true;
         pw.println("Du er forbundet til chatrummet");
 
-
         while(keepRunning) {
 
             String message = scanner.nextLine();
-            keepRunning = commandHandler(message);
-
+            try {
+                keepRunning = commandHandler(message);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
         pw.println("Du har lukket forbindelsen til chatrummet");
     }
 
-    private boolean commandHandler(String msg){
+    private boolean commandHandler(String msg) throws InterruptedException {
 
         String[] messageSplit = msg.split("#");
 
         if (messageSplit.length == 1){
+            String command = messageSplit[0];
+            switch (command){
+                case "CLOSE":
+                    //Stops while loop and closes connection
+                    return false;
+                default:
+                    throw new IllegalArgumentException("Sent request does not obey protocol");
+            }
 
         }else if (messageSplit.length == 2){
+            String command = messageSplit[0];
+            String token = messageSplit[1];
+            switch (command){
+                case "CONNECT":
+                    //Adds username to ArrayBlockingQueue in Server class
+                    userList.put(ip,token);
+                    break;
+
+                default:
+                    throw new IllegalArgumentException("Sent request does not obey protocol");
+            }
 
         }else if (messageSplit.length == 3){
+            String command = messageSplit[0];
+            String argument = messageSplit[1];
+            String message = messageSplit[2];
 
+            switch (command){
+                case "SEND":
+                    if(!message.equals("*")){
+                        String[] users = argument.split(",");
+                        server.sendToSpecificUsers(message,users);
+
+                    }else if (message.equals("*")){
+                        server.sendToAllUser(message);
+                    }
+                    break;
+
+                default:
+                    throw new IllegalArgumentException("Sent request does not obey protocol");
+            }
         }
 
         return true;
--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -3,23 +3,38 @@ package server;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Server {
 
     private ServerSocket serverSocket;
     private Socket socket;
 
+    public void sendToSpecificUsers(String message, String[] users){
+
+
+    }
+
+    public void sendToAllUser(String message){
+
+
+    }
+
     public void startSever(int port,String ip) throws IOException {
 
         serverSocket = new ServerSocket(port);
         System.out.println("Server started, listening on : " + port);
+
+        ConcurrentHashMap<String,String> userList = new ConcurrentHashMap<>(10);
+
 
         while (true) {
             System.out.println("Waiting for a client");
             socket = serverSocket.accept();      //Blocking call
             System.out.println("New client connected");
 
-            ClientHandler clientHandler = new ClientHandler(socket,ip);
+            ClientHandler clientHandler = new ClientHandler(socket,ip,userList,this);
 
             Thread thread = new Thread(clientHandler);
             thread.start();
