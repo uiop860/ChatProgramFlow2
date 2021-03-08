604bd017087c4e34e6c26bbde9203ac76da411c5
Sebastian Hejlesen <Cph-sh528@cphbusiness.dk>
2021-03-08 09:55:28 +0100

    git merge

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -1,8 +1,10 @@
 package server;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -10,31 +12,29 @@ public class Server {
 
     private ServerSocket serverSocket;
     private Socket socket;
+    ConcurrentHashMap<String, String> userList = new ConcurrentHashMap<>(10);
 
-    public void sendToSpecificUsers(String message, String[] users){
 
-
-    }
-
-    public void sendToAllUser(String message){
+    public void sendToSpecificUsers(String message, String[] users) {
 
 
     }
 
-    public void startSever(int port,String ip) throws IOException {
+    public void sendToAllUser(String message) {
+
+    }
+
+
+    public void startSever(int port, String ip) throws IOException {
 
         serverSocket = new ServerSocket(port);
         System.out.println("Server started, listening on : " + port);
-
-        ConcurrentHashMap<String,String> userList = new ConcurrentHashMap<>(10);
-
-
         while (true) {
             System.out.println("Waiting for a client");
             socket = serverSocket.accept();      //Blocking call
             System.out.println("New client connected");
 
-            ClientHandler clientHandler = new ClientHandler(socket,ip,userList,this);
+            ClientHandler clientHandler = new ClientHandler(socket, ip, userList, this);
 
             Thread thread = new Thread(clientHandler);
             thread.start();
