b34407640aff98fbe3984ec853139d0adad807de
Sebastian Hejlesen <Cph-sh528@cphbusiness.dk>
2021-03-08 13:02:41 +0100

    Handlertest updated

--- src/main/java/Test/ClientHandlerTest.java
+++ src/main/java/Test/ClientHandlerTest.java
@@ -1,26 +1,16 @@
 package Test;
 
 
-import org.junit.jupiter.api.Test;
 import server.Server;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.net.Socket;
-import java.util.Scanner;
-
-import static junit.framework.Assert.assertEquals;
-
-
 
 
 
 class ClientHandlerTest {
 
 
-
-
-    public static void main(String[]args){
+    public static void main(String[] args) {
         try {
             Runtestserver();
 
@@ -34,12 +24,10 @@ class ClientHandlerTest {
         MyRunnable testrunnable = new MyRunnable();
         Thread testThread = new Thread(testrunnable);
         testThread.start();
-        String ip = "localhost";
         int port = 8088;
         String logFile = "log.txt";
         Server server = new Server();
-        server.startSever(port, ip);
-
+        server.startSever(port);
 
 
     }
--- src/main/java/Test/MyRunnable.java
+++ src/main/java/Test/MyRunnable.java
@@ -1,44 +1,43 @@
 package Test;
 
+import org.junit.jupiter.api.Assertions;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
 
-import static junit.framework.Assert.assertEquals;
 
 public class MyRunnable implements Runnable {
 
 
-        @Override
-        public void run() {
-                try {
-                        String ip = "localhost";
-                        int port = 8088;
-                        Socket socket = new Socket(ip, port);
-                        Scanner scanner = new Scanner(socket.getInputStream());
-                        PrintWriter pw = new PrintWriter(socket.getOutputStream(), true);
+    @Override
+    public void run() {
+        try {
+            String ip = "localhost";
+            int port = 8088;
+            Socket socket = new Socket(ip, port);
+            Scanner scanner = new Scanner(socket.getInputStream());
+            PrintWriter pw = new PrintWriter(socket.getOutputStream(), true);
 
 
-                        String received = scanner.nextLine();
-                        assertEquals("Indtast CONNECT#XXXX", received);
-                        pw.println("CONNECT#Sebastian");
-                        String received1 = scanner.nextLine();
-                        assertEquals("Du er forbundet til chatrummet",received1);
-                        String received2 = scanner.nextLine();
-                        assertEquals("#send2", received2);
+            String received = scanner.nextLine();
+            Assertions.assertEquals("Indtast CONNECT#XXXX", received);
+            pw.println("CONNECT#Sebastian");
+            String received1 = scanner.nextLine();
+            Assertions.assertEquals("ONLINE#Sebastian,", received1);
+            String received2 = scanner.nextLine();
+            Assertions.assertEquals("Du er forbundet til chatrummet", received2);
+            String received3 = scanner.nextLine();
+            pw.println("");
+            Assertions.assertEquals();
 
 
-
-
-
-
-                        System.out.println("test passed");
-                } catch (IOException e) {
-                        e.printStackTrace();
-                }
+            System.out.println("test passed");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+    }
 
 
-
-        }
+}
