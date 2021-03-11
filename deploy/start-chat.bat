124f4c654dc7d4f2e8beff9775da6a519c0cb306
Oliver Stæhr <oliverstaehr96@gmail.com>
2021-03-11 13:37:34 +0100

    Adds .bat to gitignore

--- deploy/start-chat.bat
+++
@@ -1,1 +0,0 @@
-java -cp chat-server.jar client.ChatClientMain 188.166.162.188 2345
--- deploy/start-server.bat
+++
@@ -1,3 +0,0 @@
-java -cp chat-server.jar server.ChatServerMain
-
-java -cp chat-client-server.jar server.ChatServer
