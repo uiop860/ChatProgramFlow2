f812889c893796e49146d1a758fb3dfdb23719f8
Sebastian Hejlesen <Cph-sh528@cphbusiness.dk>
2021-03-11 22:04:10 +0100

    more users added

--- src/main/java/server/MessageHandler.java
+++ src/main/java/server/MessageHandler.java
@@ -1,10 +1,12 @@
 package server;
 
 
+
+
 import java.util.NoSuchElementException;
 import java.util.concurrent.ConcurrentHashMap;
 
-public class  MessageHandler {
+public class MessageHandler {
     private ConcurrentHashMap<String, ClientHandler> userList;
     private ClientHandler myClientHandler;
 
@@ -31,41 +33,61 @@ public class  MessageHandler {
             String argument = messageSplit[1];
             String message = messageSplit[2];
 
+
             switch (command) {
                 case "SEND":
                     if (argument.equals("*")) {
                         sendToAllUser(message, myName);
                     } else {
-                        sendToSpecificUser(message, myName, argument);
-                    }
-                    break;
-                default:
+                        String[] parts = argument.split(",");
+                        if (parts.length == 1) {
+                            sendToSpecificUser(message, myName, argument);
+                        } else {
+                            sendToUsers(message, myName, parts);
+                        }
+                        break;
 
+
+
+
+                    }
+                default:
                     myClientHandler.writeToClient("unknown command");
 
             }
+
         }
         return true;
     }
+        public void sendToUsers (String message, String sender, String[]receivers){
 
-    public void sendToAllUser(String message, String name) {
-        userList.values().forEach(clientHandler -> {
-            clientHandler.messageToAll(message, name);
-        });
-    }
+            for (int i = 0; i < receivers.length; i++) {
+                if (userList.containsKey(receivers[i])) {
 
-    public void sendOnlineMessage() {
+                    userList.get(receivers[i]).messageToAll(message, sender);
 
-        userList.values().forEach(clientHandler -> {
-            clientHandler.sendOnlineMesage();
-        });
-    }
+                }
+            }
+        }
 
-    public void sendToSpecificUser(String message, String name, String user) {
-        if (userList.containsKey(user)){
-            userList.get(user).messageToAll(message, name);
-        } else {
-            throw new NoSuchElementException();
+        public void sendToAllUser (String message, String name){
+            userList.values().forEach(clientHandler -> {
+                clientHandler.messageToAll(message, name);
+            });
+        }
+
+        public void sendOnlineMessage () {
+
+            userList.values().forEach(clientHandler -> {
+                clientHandler.sendOnlineMesage();
+            });
+        }
+
+        public void sendToSpecificUser (String message, String name, String user){
+            if (userList.containsKey(user)) {
+                userList.get(user).messageToAll(message, name);
+            } else {
+                throw new NoSuchElementException();
+            }
         }
     }
-}
